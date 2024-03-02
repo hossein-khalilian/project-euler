157c232db1b1e7b5ac71432da6848de4fa73c7e3
hossein <hse.khalilian08@gmail.com>
2024-03-02 16:00:09 +0330

    add a better solution for problem 2 in golang

--- golang/problem02/main.go
+++ golang/problem02/main.go
@@ -29,6 +29,26 @@ func get_fibonachi_sum() int{
 }
 
 
+func get_fibonachi_sum1() int{
+    sum := 0
+    fib := []int{1, 1}
+    for {
+        fib[len(fib)-1] = fib[len(fib)-1] + fib[len(fib)-2]
+        fib[len(fib)-2] = fib[len(fib)-1] - fib[len(fib)-2]
+        if fib[len(fib)-1] > 4000000{
+            break
+        }
+        if fib[len(fib)-1] % 2 == 0{
+            sum += fib[len(fib)-1]
+        }
+    }
+    // fmt.Println(fib)
+    return sum
+}
+
+
+
 func main(){
     fmt.Println(get_fibonachi_sum())
+    fmt.Println(get_fibonachi_sum1())
 }
