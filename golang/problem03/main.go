dd812341425a46f58d5a61cf7a372efa1707783a
hossein <hse.khalilian08@gmail.com>
2024-03-03 12:59:07 +0330

    show runing time

--- golang/problem03/main.go
+++ golang/problem03/main.go
@@ -3,9 +3,10 @@ package main
 import (
     "fmt"
     "math"
+    "time"
 )
 
-func get_primes(n int) []int{
+func get_factors(n int) []int{
     root := int(math.Sqrt(float64(n)))
     factors := make([]int, 0)
     for i := range root{
@@ -20,13 +21,13 @@ func get_primes(n int) []int{
 }
 
 func main(){
-    // fmt.Println(get_primes(13195))
-
+    // fmt.Println(get_factors(13195))
+    now := time.Now()
     prime_factors := make([]int, 0)
-    factors := get_primes(600851475143)
+    factors := get_factors(600851475143)
     maxprime := 0
     for _, i := range(factors){
-        if len(get_primes(i)) == 2{
+        if len(get_factors(i)) == 2{
             if i > maxprime{
                 maxprime = i
             }
@@ -34,5 +35,6 @@ func main(){
         }
 
     }
+    fmt.Println(time.Since(now))
     fmt.Println(maxprime)
 }
